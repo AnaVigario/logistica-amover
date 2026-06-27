0bce3db553a07c1488108d9bf8a0a8a187fa136e
DDias540 <davidrdias540@gmail.com>
2026-06-27 17:32:34 +0100

    Last updates - Integration

--- backend/obj/Release/net8.0/AMoVeR-Logistica.AssemblyInfo.cs
+++ backend/obj/Release/net8.0/AMoVeR-Logistica.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AMoVeR-Logistica")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29dbac8b51ba59d497fa14858a68ce0100a136e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AMoVeR-Logistica")]
